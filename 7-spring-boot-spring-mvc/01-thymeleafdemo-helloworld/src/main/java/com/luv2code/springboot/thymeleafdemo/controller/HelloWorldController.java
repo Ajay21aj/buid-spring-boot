197e78ebf9e5b2b957c19d66da3a6a63b7798789
Ajay21aj <ajaykyadav3035@gmail.com>
2024-04-28 10:21:28 +0530

    Exploring the Model,controller and view in spring mvc with the help of the thymeleaf here we are trying to implement data binding

--- 7-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 7-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -4,12 +4,15 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
     // need a controller method to show initial HTML form
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
@@ -31,6 +34,26 @@ public class HelloWorldController {
 
         // create the message
         String result = "Yo "+ theName;
+
+        // add message to the model
+        model.addAttribute("message",result);
+
+
+
+        return "helloWorld";
+
+
+    }
+    @PostMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+        // read the request parameter from the HTML form
+
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey my friend from v3! "+ theName;
 
         // add message to the model
         model.addAttribute("message",result);
